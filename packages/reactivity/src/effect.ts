950b8da21b99154d7431d8db5bed4764f8ff0070
if00 <123@qq.com>
2023-07-08 17:19:05 +0800

    更新

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,15 +1,18 @@
-
 export function effect<T = any>(fn: () => T) {
   const _effect = new ReactiveEffect(fn)
+  _effect.run()
 }
+
+export let activeEffect: ReactiveEffect | undefined
 
 export class ReactiveEffect<T = any> {
   constructor(public fn: () => T) {}
 
   run() {
+    activeEffect = this
+
     return this.fn()
   }
-
 }
 
 /**
