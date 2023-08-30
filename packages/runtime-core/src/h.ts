d720cf4c99b8de24e81c6b535a55e9ffb3fe88e7
if00 <123@qq.com>
2023-08-30 21:28:12 +0800

    m

--- packages/runtime-core/src/h.ts
+++ packages/runtime-core/src/h.ts
@@ -1,1 +1,10 @@
-export function h(type: any, propsOrChildren?: any, children?: any) {}
+import { isArray, isObject } from '@vue/shared'
+import { VNode } from './vnode'
+
+export function h(type: any, propsOrChildren?: any, children?: any): VNode {
+  const l = arguments.length
+  if (l === 2) {
+    if (isObject(propsOrChildren) && !isArray(propsOrChildren)) {
+    }
+  }
+}
