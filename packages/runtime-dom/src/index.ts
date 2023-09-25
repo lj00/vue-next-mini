0c6206788d449ddad61e198c91292f2d5362090a
if00 <123@qq.com>
2023-09-25 21:51:44 +0800

    m

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,1 +1,16 @@
-export const redner
+import { extend } from '@vue/shared'
+import { createRenderer } from 'packages/runtime-core/src/renderer'
+import { nodeOps } from './nodeOps'
+import { patchProp } from './patchProp'
+
+const rendererOptions = extend({ patchProp }, nodeOps)
+
+let renderer
+
+function ensureRenderer() {
+  return renderer || (renderer = createRenderer(rendererOptions))
+}
+
+export const redner = (...args) => {
+  ensureRenderer().render
+}
