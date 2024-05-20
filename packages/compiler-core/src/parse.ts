74389d091fbb87c29ae1394bcb0d13a8b5a5c91e
if00 <123@qq.com>
2024-05-20 22:02:03 +0800

    m

--- packages/compiler-core/src/parse.ts
+++ packages/compiler-core/src/parse.ts
@@ -1,3 +1,10 @@
+import { ElementTypes, NodeTypes } from './ast'
+
+const enum TagType {
+  Start,
+  End
+}
+
 export interface ParserContext {
   source: string
 }
@@ -17,9 +24,91 @@ export function baseParse(content: string) {
 function parseChildren(context: ParserContext, ancestors) {
   const nodes = []
 
+  while (!isEnd(context, ancestors)) {
+    const s = context.source
+    let node
+    if (startsWith(s, '{{')) {
+      // TODO: {{
+    } else if (s[0] === '<') {
+      if (/[a-z]/i.test(s[1])) {
+        node = parseElement(context, ancestors)
+      }
+    }
+
+    if (!node) {
+      node = parseText(context)
+    }
+
+    pushNode(nodes, node)
+  }
+
   return nodes
+}
+
+function pushNode(nodes, node) {
+  nodes.push(node)
+}
+
+function parseElement(context: ParserContext, ancestors) {
+  const element = parseTag(context, TagType.Start)
+
+  ancestors.push(element)
+  const children = parseChildren(context, ancestors)
+  ancestors.pop()
+
+  element.children = children
+
+  if (startsWithEndTagOpen(context.source, element.tag)) {
+    parseTag()
+  }
+}
+
+function parseText(context: ParserContext) {}
+
+function parseTag(context: ParserContext, type: TagType) {
+  const match = /^<\/?([a-z][^\t\r\n\f />]*)/i.exec(context.source)!
+  const tag = match[1]
+
+  advanceBy(context, match[0].length)
+
+  let isSelfClosing = startsWith(context.source, '/>')
+  advanceBy(context, isSelfClosing ? 2 : 1)
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+    tagType: ElementTypes.ELEMENT,
+    children: [],
+    props: []
+  }
+}
+
+function isEnd(context: ParserContext, ancestors) {
+  const s = context.source
+  if (startsWith(s, '</')) {
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      if (startsWithEndTagOpen(s, ancestors[i].tag)) {
+        return true
+      }
+    }
+  }
+
+  return !s
 }
 
 function startsWith(source: string, searchString: string): boolean {
   return source.startsWith(searchString)
+}
+
+function startsWithEndTagOpen(source: string, tag: string): boolean {
+  return (
+    startsWith(source, '</') &&
+    source.slice(2, 2 + tag.length).toLowerCase() == tag.toLowerCase() &&
+    /[\t\r\n\f />]/.test(source[2 + tag.length] || '>')
+  )
+}
+
+function advanceBy(context: ParserContext, numberOfCharacters: number) {
+  const { source } = context
+  context.source = source.slice(numberOfCharacters)
 }
