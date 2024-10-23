b15ddabe0de78148ec215614a35e72f26d72d616
LJ_00 <1058996144@qq.com>
2024-10-23 18:17:31 +0800

    m

--- packages/compiler-core/src/compile.ts
+++ packages/compiler-core/src/compile.ts
@@ -1,9 +1,18 @@
+import { extend } from '@vue/shared'
 import { baseParse } from './parse'
+import { transform } from './transform'
+import { transformElement } from './transforms/transformElement'
+import { transformText } from './transforms/transformText'
 
 export function baseCompile(template: string, options) {
   const ast = baseParse(template)
 
-  console.log(JSON.stringify(ast))
+  transform(
+    ast,
+    extend(options, {
+      nodeTransforms: [transformElement, transformText]
+    })
+  )
 
   return {}
 }
---
+++ packages/compiler-core/src/transform.ts
@@ -0,0 +1,1 @@
+export function transform(root, options) {}
