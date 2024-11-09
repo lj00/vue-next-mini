e9619168791e0a70d0ba8e96cebdc812f6acc87a
if00 <123@qq.com>
2024-11-09 22:35:30 +0800

    m

--- packages/compiler-core/src/codegen.ts
+++ packages/compiler-core/src/codegen.ts
@@ -1,7 +1,36 @@
 import { helperNameMap } from './runtimeHelpers'
 
+const aliasHelper = (s: symbol) => `${helperNameMap[s]}: _${helperNameMap[s]}`
+
 export function generate(ast) {
   const context = createCodegenContext(ast)
+
+  const { push, newline, indent, deindent } = context
+
+  genFunctionPreamble(context)
+
+  const functionName = `render`
+  const args = ['_ctx', '_cache']
+  const signature = args.join(', ')
+  push(`function ${functionName}(${signature}) {`)
+
+  const hasHelpers = ast.helpers.length > 0
+  if (hasHelpers) {
+    push(`const { ${ast.helpers.map(aliasHelper).join(', ')} } = _Vue`)
+    push(`\n`)
+    newline()
+  }
+
+  newline()
+  push(`return `)
+}
+
+function genFunctionPreamble(context) {
+  const { push, runtimeGlobalName, newline } = context
+  const VueBinding = runtimeGlobalName
+  push(`const _Vue = ${VueBinding}\n`)
+  newline()
+  push(`return `)
 }
 
 function createCodegenContext(ast) {
